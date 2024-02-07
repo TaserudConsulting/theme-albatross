fa238017877d19acdb7dc755ba3d220ddd087109
Elis Hirwing <elis@hirwing.se>
2024-02-07 18:29:49 +0100

    fix: Small fixes to the flake

--- flake.nix
+++ flake.nix
@@ -21,67 +21,63 @@
     # Specify formatter package for "nix fmt ." and "nix fmt . -- --check"
     formatter = pkgs.alejandra;
 
-    # Expose the web iso flags for esay access.
-    packages.web-iso-flags = inputs.web-iso-flags.packages.${system}.default;
+    packages = {
+      # Expose the web iso flags for esay access.
+      web-iso-flags = inputs.web-iso-flags.packages.${system}.default;
 
-    # Expose the version of hugo used to build the theme along with
-    # extra dependencies.
-    packages.hugo = pkgs.symlinkJoin {
-      name = "hugo-${pkgs.hugo.version}-dart-sass-embedded-${pkgs.dart-sass.version}-bundle";
+      # Expose the version of hugo used to build the theme along with
+      # extra dependencies.
+      hugo = pkgs.symlinkJoin {
+        name = "hugo-${pkgs.hugo.version}-dart-sass-embedded-${pkgs.dart-sass.version}-bundle";
 
-      buildInputs = [pkgs.makeWrapper];
-      paths = [pkgs.hugo pkgs.dart-sass];
+        buildInputs = [pkgs.makeWrapper];
+        paths = [pkgs.hugo pkgs.dart-sass];
 
-      postBuild = "wrapProgram $out/bin/hugo --prefix PATH : ${pkgs.dart-sass}/bin";
+        postBuild = "wrapProgram $out/bin/hugo --prefix PATH : ${pkgs.dart-sass}/bin";
 
-      meta.mainProgram = "hugo";
-    };
-
-    # Expose the theme files.
-    packages.theme = pkgs.stdenv.mkDerivation {
-      pname = "taserud-consulting-ab--theme-albatross";
-      inherit version;
-
-      src = ./src;
-
-      passthru.theme-name = "albatross";
-
-      installPhase = ''
-        cp -avr . $out
-
-        # Install iso flags resources
-        install -m 644 -D ${self.packages.${system}.web-iso-flags}/* -t $out/static/img/iso-flags
-
-        # Install fontawesome resources
-        install -m 644 -D ${self.packages.${system}.fontawesome}/scss/* -t $out/assets/scss/fontawesome
-        install -m 644 -D ${self.packages.${system}.fontawesome}/webfonts/* -t $out/static/fonts/fontawesome
-      '';
-    };
-
-    packages.fontawesome = let
-      version = "6.5.1";
-    in pkgs.stdenv.mkDerivation {
-      pname = "fontawesome-free";
-      inherit version;
-
-      src = pkgs.fetchzip {
-        url = "https://use.fontawesome.com/releases/v${version}/fontawesome-free-${version}-web.zip";
-        hash = "sha256-gXXhKyTDC/Q6PBzpWRFvx/TxcUd3msaRSdC3ZHFzCoc=";
+        meta.mainProgram = "hugo";
       };
 
-      buildPhase = ":";
+      # Expose the theme files.
+      theme = pkgs.stdenv.mkDerivation {
+        name = "taserud-consulting-ab--theme-albatross";
+        passthru.theme-name = "albatross";
 
-      installPhase = ''
-        mkdir -p $out
+        src = ./src;
 
-        cp -vr scss webfonts $out
-      '';
+        installPhase = ''
+          cp -avr . $out
+
+          # Install iso flags resources
+          install -m 644 -D ${self.packages.${system}.web-iso-flags}/* -t $out/static/img/iso-flags
+
+          # Install fontawesome resources
+          install -m 644 -D ${self.packages.${system}.fontawesome}/scss/* -t $out/assets/scss/fontawesome
+          install -m 644 -D ${self.packages.${system}.fontawesome}/webfonts/* -t $out/static/fonts/fontawesome
+        '';
+      };
+
+      # Download and build fontawesome resources.
+      fontawesome = let
+        pname = "fontawesome-free";
+        version = "6.5.1";
+      in
+        pkgs.stdenv.mkDerivation {
+          inherit pname version;
+
+          src = pkgs.fetchzip {
+            url = "https://use.fontawesome.com/releases/v${version}/${pname}-${version}-web.zip";
+            hash = "sha256-gXXhKyTDC/Q6PBzpWRFvx/TxcUd3msaRSdC3ZHFzCoc=";
+          };
+
+          buildPhase = ":";
+          installPhase = "mkdir -p $out && cp -vr scss webfonts $out";
+        };
     };
 
     # A test case to see that the theme builds.
     checks.test = pkgs.stdenv.mkDerivation {
-      pname = "taserud-consulting-ab-theme-albatross-test";
-      inherit version;
+      name = "taserud-consulting-ab-theme-albatross-test";
 
       src = ./.;
 
